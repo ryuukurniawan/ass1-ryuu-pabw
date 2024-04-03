6106dffa77195ec3e8277068723ce2720d83b7c6
Muhammad Ryuu Kurniawan <ryuuryuu048@gmail.com>
2024-04-03 13:31:16 +0700

    menambahkan array ke file script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
+let buku = [
+    {"nama": "sherlock", "jumlah": 2},
+    {"nama": "lupin", "jumlah": 2},
+    {"nama": "aladin", "jumlah": 2},
+];
 
 function cekBuku() {
     let jumlah = document.getElementById("jumlah").value;
